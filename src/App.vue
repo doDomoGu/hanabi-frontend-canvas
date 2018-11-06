66aa71fb48862b9d0d1206001ae0e833ba85e31d
doDomoGu <kaka_71936410@qq.com>
2018-11-06 13:00:21 +0800

    canvas绘制

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <canvas id="canvas"></canvas>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyCanvas from './assets/MyCanvas'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      form: {
+        username: 'admin',
+        password: '123123'
+      },
+      errormsg: ''
+    }
+  },
+  mounted: function () {
+
+    this.canvas = document.querySelector('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.ratio = MyCanvas.getPixelRatio(this.ctx)
+//    console.log(this.ratio)
+
+//    this.canvas.width = window.innerWidth * this.ratio
+//    this.canvas.height = (window.innerHeight /*- MyCanvas.px2Rem(40)*/) * this.ratio
+
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
+
+    this.ctx.fillStyle = '#eeeeee' // 屏幕背景色
+    //this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height )
+
+    console.log(window.innerWidth)
+    console.log(window.innerHeight)
+
+    this.ctx.fillStyle = '#008800'
+    let rect = {
+      x:100,
+      y:100,
+      h:100,
+      w:100
+    }
+    MyCanvas.drawRoundedRect(rect, 2, this.ctx)
+
+
+    //this.$store.dispatch('common/SetTitle2', '登录')
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html, body {
+        margin:0;
+        padding:0;
+        font-size: 0;
+    }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// import common from './common'
+//
+// import auth from './modules/auth'
+//
+// import room from './modules/room'
+//
+// import myRoom from './modules/myRoom'
+// import myGame from './modules/myGame'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    // common,
+    // auth,
+    // room,
+    // myRoom,
+    // myGame
+  }
+})
+
