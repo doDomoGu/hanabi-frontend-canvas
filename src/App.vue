9b6edb45c6a505ffb567406d31f10771903f0609
doDomoGu <kaka_71936410@qq.com>
2018-11-11 16:03:20 +0800

    调整流程  增加 moment

--- src/App.vue
+++ src/App.vue
@@ -15,186 +15,137 @@
 </template>
 
 <script>
-import MyCanvas from './assets/js/MyCanvas.js'
-import Login    from './components/Login.vue'
-import RoomList    from './components/RoomList.vue'
+    import moment from 'moment'
+    import MyCanvas from './assets/js/MyCanvas.js'
+    import Login    from './components/Login.vue'
+    import RoomList    from './components/RoomList.vue'
 
-export default {
-    name: 'app',
-    components: { Login, RoomList },
-    data() {
-        return {
-            isLoginLoading : false
-        }
-    },
-    computed: {
-        isLogin(){
-            return this.$store.getters['user/isLogin']
+    export default {
+        name: 'app',
+        components: { Login, RoomList },
+        data() {
+            return {
+                isLoginLoading : false
+            }
         },
-        isInRoom(){
-            return this.$store.getters['myRoom/roomId'] > 0
+        computed: {
+            isLogin(){
+                return this.$store.getters['user/isLogin']
+            },
+            isInRoom(){
+                return this.$store.getters['myRoom/roomId'] > 0
+            },
+            isInGame(){
+                return this.$store.getters['myGame/isPlaying']
+            },
         },
-        isInGame(){
-            return this.$store.getters['myGame/isPlaying']
-        },
-    },
-    watch: {
-        isLogin(newVal) {
-            if(newVal){
-                //登录后，非刷新页面，进入初始化流程
-                if(!this.isLoginLoading){
+        watch: {
+            isLogin(newVal) {
+                if(newVal){
+                    //登录后，非刷新页面，进入初始化流程
+                    if(!this.isLoginLoading){
+                        this.init()
+                    }
+                }else {
+                    //登出，进入初始化流程
                     this.init()
                 }
-            }else {
-                //登出，进入初始化流程
+            }/*,
+            roomList(newVal, oldVal) {
+                console.log(newVal)
+                console.log(oldVal)
+            }*/
+        },
+        mounted() {
+            //let that = this
+
+            this.c_bottom = document.querySelector('#c_bottom')
+            this.ctx_bottom = this.c_bottom.getContext('2d')
+
+            /*this.c_m = document.querySelector('#c_middle')
+            this.ctx_m = this.c_m.getContext('2d')*/
+
+            //this.ratio = window.devicePixelRatio //MyCanvas.getPixelRatio(this.ctx)
+
+
+            this.clearCanvas()
+            this.drawBottom('#d9f1f8')
+
+            this.checkToken().then(()=>{
                 this.init()
+              }
+            ).catch(()=>{
+                //
+            })
+
+            /*this.ctx.fillStyle = '#008800'
+            let rect = {
+                x:100,
+                y:100,
+                h:100,
+                w:100
             }
-        }/*,
-        roomList(newVal, oldVal) {
-            console.log(newVal)
-            console.log(oldVal)
-        }*/
-    },
-    mounted() {
-        //let that = this
+            MyCanvas.drawRoundedRect(rect, 2, this.ctx)*/
 
-        this.c_bottom = document.querySelector('#c_bottom')
-        this.ctx_bottom = this.c_bottom.getContext('2d')
+        },
+        methods: {
+            init(){
+                console.log(' ')
+                console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
+                console.log('app init')
 
-        /*this.c_m = document.querySelector('#c_middle')
-        this.ctx_m = this.c_m.getContext('2d')*/
+                this.clearCanvas()  //初始化 画板
+                this.drawBottom('#d9f1f8')  //绘制背景图
 
-        //this.ratio = window.devicePixelRatio //MyCanvas.getPixelRatio(this.ctx)
-
-
-        this.clearCanvas()
-        this.drawBottom('#d9f1f8')
-
-        this.checkToken().then(()=>{
-            this.init()
-          }
-        ).catch(()=>{
-            console.log('not login')
-        })
-
-        /*let colors = [
-          '#4ef86d',
-          '#f8dd7e'
-        ]
-
-        let y = 0
-        let x = 0
-            that.c_m.width = window.innerWidth
-            that.c_m.height = window.innerHeight
-        setInterval(function(){
-//            that.c_m.width = window.innerWidth
-//            that.c_m.height = window.innerHeight
-            that.ctx_m.fillStyle = colors[Math.round(Math.random())]
-            that.ctx_m.fillRect(x, y , 30, 30 )
-
-            x += Math.round(Math.random()) * 1 + Math.round(Math.random()) * 2 + Math.round(Math.random()) * 1
-            y += Math.round(Math.random()) * 1 + Math.round(Math.random()) * 2 + Math.round(Math.random()) * 5
-        },10)*/
-
-
-
-        /*this.ctx.fillStyle = '#008800'
-        let rect = {
-            x:100,
-            y:100,
-            h:100,
-            w:100
-        }
-        MyCanvas.drawRoundedRect(rect, 2, this.ctx)*/
-
-    },
-    methods: {
-        init(){
-            this.clearCanvas()  //初始化 画板
-            this.drawBottom('#d9f1f8')  //绘制背景图
-
-            if(this.isLogin){
-                if(this.isInRoom){
-                    if(this.isInGame){
-                        console.log('is in game')
-                        //this.$store.dispatch('room/List')
+                if(this.isLogin){
+                    if(this.isInRoom){
+                        if(this.isInGame){
+                            console.log('is in game')
+                            //this.$store.dispatch('room/List')
+                        }else{
+                            console.log('is in room')
+                            //this.$store.dispatch('room/List')
+                        }
                     }else{
-                        console.log('is in room')
+                        console.log('is not in room')
+                        //this.drawRoomList()
                         //this.$store.dispatch('room/List')
                     }
                 }else{
-                    console.log('is not in room')
-                    //this.drawRoomList()
-                    this.$store.dispatch('room/List')
+                  console.log('not login')
                 }
-            }
-        },
-        clearCanvas(){
-            this.c_bottom.width = window.innerWidth
-            this.c_bottom.height = window.innerHeight
-//            this.c_m.width = window.innerWidth
-//            this.c_m.height = window.innerHeight
-        },
-        drawBottom(bgColor) {
-            this.ctx_bottom.fillStyle = bgColor // 屏幕背景色
-            this.ctx_bottom.fillRect(0, 0, this.c_bottom.width, this.c_bottom.height )
-        },
-        checkToken() {
-            let that = this
-            this.isLoginLoading = true
-            return new Promise (function(resolve, reject) {
-                let token = localStorage.getItem('__HANABI_AUTH_TOKEN__') || null
-                if(token!== null ){
-                    that.$store.dispatch('user/CheckToken', token).then( ()=>{
-                        that.isLoginLoading = false
-                        if(that.$store.getters['user/isLogin']){
+            },
+            clearCanvas(){
+                this.c_bottom.width = window.innerWidth
+                this.c_bottom.height = window.innerHeight
+    //            this.c_m.width = window.innerWidth
+    //            this.c_m.height = window.innerHeight
+            },
+            drawBottom(bgColor) {
+                this.ctx_bottom.fillStyle = bgColor // 屏幕背景色
+                this.ctx_bottom.fillRect(0, 0, this.c_bottom.width, this.c_bottom.height )
+            },
+            checkToken() {
+                let that = this
+                this.isLoginLoading = true
+                return new Promise (function(resolve, reject) {
+                    let token = localStorage.getItem('__HANABI_AUTH_TOKEN__') || null
+                    if(token!== null ){
+                        that.$store.dispatch('user/CheckToken', token).then( ()=>{
+                            that.isLoginLoading = false
                             resolve()
-                        }else{
-                            reject()
-                        }
-                    })
-                } else {
-                    that.isLoginLoading = false
-                    reject()
-                }
-            })
-        },
-        /*drawRoomList(){
-            let that = this
-            this.c_m.width = window.innerWidth
-            this.c_m.height = window.innerHeight
-
-            this.$store.dispatch('room/List').then(res=>{
-                let x = 30
-                let y = 30
-                let w = this.c_m.width - x * 2
-                let h = 40
-
-                res.forEach((r,i)=>{
-                    let y2 = y + ( h + 20 ) * i
-
-                    that.ctx_m.fillStyle = '#fffefd'
-                    that.ctx_m.fillRect(x, y2, w, h)
-
-
-                    that.ctx_m.font = MyCanvas.px2Rem(30) + 'px Microsoft JhengHei'
-                    that.ctx_m.fillStyle = '#8a8a8a'
-                    that.ctx_m.textAlign = 'left'
-                    that.ctx_m.textBaseline = 'middle'
-
-                    let str = r.id + '. ' + r.title
-                    that.ctx_m.fillText(str, x + 10, y2 + h  / 2)
-
-
-
+                        })
+                    } else {
+                        that.isLoginLoading = false
+                        reject()
+                    }
                 })
-            })
-        },*/
-        logout(){
-            this.$store.dispatch('user/Logout')
+            },
+            logout(){
+                this.$store.dispatch('user/Logout')
+            }
         }
     }
-}
 </script>
 <style>
     html, body {
@@ -205,7 +156,7 @@ export default {
     }
     #c_bottom,
     #c_middle,
-    #c_room_list,
+    /*#c_room_list,*/
     #c_top {
         position: absolute;
     }
--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -2,84 +2,104 @@
     <canvas id="canvas_room_list"></canvas>
 </template>
 <script>
-import MyCanvas from '../assets/js/MyCanvas.js'
 
-export default {
-    name: 'room_list',
-    data() {
-        return {
-          intervalId : null
-        }
-    },
-    computed:{
-        roomList(){
-            return this.$store.getters['room/list']
-        }
-    },
-    watch: {
-        roomList(newVal, oldVal) {
-            console.log(newVal)
-            console.log(oldVal)
+    import moment   from 'moment'
 
-            if(newVal != oldVal){
-//                console.log(this.canvas)
-//                console.log(this.ctx)
+    import MyCanvas from '../assets/js/MyCanvas.js'
 
-                this.draw(this.ctx, newVal)
+    export default {
+        name: 'room_list',
+        data() {
+            return {
+              intervalId : null
             }
-        }
-    },
-    mounted() {
-        console.log('mounted')
-        this.canvas = document.querySelector('#canvas_room_list')
-        this.ctx = this.canvas.getContext('2d')
-
-        this.intervalId = setInterval(()=>{
-            if(this.$store.getters['user/isLogin']){
-                this.$store.dispatch('room/List')
+        },
+        computed:{
+            roomList(){
+                return this.$store.getters['room/list']
             }
-        },1000)
-//        this.clearCanvas(this.canvas)
-
-
-    },
-    methods: {
-        //设置canvas宽高 并清空内容
-        clearCanvas(canvas) {
-            canvas.width = window.innerWidth
-            canvas.height = window.innerHeight
         },
-        //绘制列表
-        draw(ctx, roomList){
+        watch: {
+            roomList(newVal, oldVal) {
+    //            console.log(newVal)
+    //            console.log(oldVal)
+    //            console.log(newVal != oldVal)
+    //            console.log(Object.is(newVal, oldVal))
 
-            this.clearCanvas(ctx.canvas)
+                if(newVal != oldVal){
+    //                console.log(this.canvas)
+    //                console.log(this.ctx)
 
-            let x = 30
-            let y = 30
-            let w = ctx.canvas.width - x * 2
-            let h = 40
+                    this.draw(this.ctx, newVal)
 
-            roomList.forEach((r,i)=>{
-                let y2 = y + ( h + 20 ) * i
-                ctx.fillStyle = '#fffefd'
-                ctx.fillRect(x, y2, w, h)
-
-                ctx.font = MyCanvas.px2Rem(30) + 'px Microsoft JhengHei'
-                ctx.fillStyle = '#8a8a8a'
-                ctx.textAlign = 'left'
-                ctx.textBaseline = 'middle'
-
-                let str = r.id + '. ' + r.title
-                ctx.fillText(str, x + 10, y2 + h  / 2)
-            })
+                }
+            }
         },
-    },
-    destroyed(){
-      console.log('destroyed')
-      console.log(this.intervalId)
-      clearInterval(this.intervalId)
+        mounted() {
+            console.log(' ')
+            console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
+            console.log('mounted')
+
+            this.canvas = document.querySelector('#canvas_room_list')
+            this.ctx = this.canvas.getContext('2d')
+
+            this.getList()
+            this.intervalId = setInterval(()=>{
+                this.getList()
+            },1000)
+        },
+        methods: {
+            getList(){
+                if(this.$store.getters['user/isLogin']){
+                    this.$store.dispatch('room/List')
+                }
+            },
+            //设置canvas宽高 并清空内容
+            clearCanvas(canvas) {
+                console.log(' ')
+                console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
+                console.log('clear canvas room list ')
+
+                canvas.width = window.innerWidth
+                canvas.height = window.innerHeight
+            },
+            //绘制列表
+            draw(ctx, roomList){
+                console.log(' ')
+                console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
+                console.log('draw room list')
+
+                this.clearCanvas(ctx.canvas)
+
+                let x = 30
+                let y = 30
+                let w = ctx.canvas.width - x * 2
+                let h = 40
+
+                roomList.forEach((r,i)=>{
+                    let y2 = y + ( h + 20 ) * i
+                    ctx.fillStyle = '#fffefd'
+                    ctx.fillRect(x, y2, w, h)
+
+                    ctx.font = MyCanvas.px2Rem(30) + 'px Microsoft JhengHei'
+                    ctx.fillStyle = '#8a8a8a'
+                    ctx.textAlign = 'left'
+                    ctx.textBaseline = 'middle'
+
+                    let str = r.id + '. ' + r.title
+                    ctx.fillText(str, x + 10, y2 + h  / 2)
+                })
+
+            },
+        },
+        destroyed(){
+            console.log(' ')
+            console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
+            console.log('destroyed')
+
+            clearInterval(this.intervalId)
+        }
     }
-}
 </script>
 <style scoped>
     #canvas_room_list {
