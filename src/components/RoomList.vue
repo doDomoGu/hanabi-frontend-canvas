0381e1250a976721db5209b15a19397f2d414a15
doDomoGu <kaka_71936410@qq.com>
2018-11-16 01:01:47 +0800

    房间列表 触摸效果

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,5 +1,5 @@
 <template>
-    <canvas id="canvas_room_list" @click="eventListener"></canvas>
+    <canvas id="canvas_room_list" ></canvas>
 </template>
 <script>
 import moment   from 'moment'
@@ -10,7 +10,8 @@ export default {
     name: 'room_list',
     data() {
         return {
-            intervalId : 0
+            intervalId : 0,
+            itemIndex : 0
         }
     },
     computed:{
@@ -51,6 +52,9 @@ export default {
         this.intervalId = setInterval(()=>{
             this.$store.dispatch('room/GetList')
         },1000)
+
+        this.canvas.addEventListener('touchstart',this.eventListener,false)
+        this.canvas.addEventListener('touchend',this.eventListener,false)
     },
     methods: {
         //设置canvas宽高 并清空内容
@@ -65,32 +69,42 @@ export default {
         //绘制列表
         draw(ctx, list){
             this.clearCanvas(ctx.canvas)
-
             //                console.log(' ')
             //                console.log(moment().format("YYYY-MM-DD HH:mm:ss SSS"))
             //                console.log('draw room list')
 
-            let x = RLCParam.listX
-            let y = RLCParam.listY
+            for(let index in list){
+                let _idx = parseInt(index) + 1
+                this.drawItem(ctx, _idx, this.itemIndex === _idx)
+
+            }
+        },
+        //绘制单个
+        drawItem(ctx, index, actived){
             let w = RLCParam.listItemW
             let h = RLCParam.listItemH
             let pad = RLCParam.listItemPad
+            let x = RLCParam.listX
+            let y = RLCParam.listY + parseInt(index - 1) * parseInt(h + pad)
+            let txt = index + '. ' + this.list[index - 1].title
+            
+            let bgcolor, txtcolor
+            if(actived){
+                bgcolor = '#333333'
+                txtcolor = '#fefefe'
+            }else{
+                bgcolor = '#fefefe'
+                txtcolor  = '#4b4b4b' 
+            }
 
-
-            list.forEach((r)=>{
-                ctx.fillStyle = '#fefefe'
-                ctx.fillRect(x, y, w, h)
-
-                ctx.font = MyCanvas.px2Rem(24) + 'px Microsoft JhengHei'
-                ctx.fillStyle = '#4b4b4b'
-                ctx.textAlign = 'left'
-                ctx.textBaseline = 'middle'
-
-                let str = r.id + '. ' + r.title
-                ctx.fillText(str, x + 10, y + h  / 2)
-
-                y += h + pad
-            })
+            ctx.fillStyle = bgcolor
+            ctx.fillRect(x, y, w, h)
+            
+            ctx.font = MyCanvas.px2Rem(24) + 'px Microsoft JhengHei'
+            ctx.fillStyle = txtcolor
+            ctx.textAlign = 'left'
+            ctx.textBaseline = 'middle'
+            ctx.fillText(txt, x + 10, y + h  / 2)
         },
         //test 绘制点击信息
         drawTest(ctx, pos, index){
@@ -108,60 +122,63 @@ export default {
             ctx.textAlign = 'left'
             ctx.textBaseline = 'middle'
 
-            let str = "坐标：" + pos.x + "," + pos.y + ' = ' + index
+            let str = "坐标：" + pos.x + "," + pos.y + ' = ' + index + ' ' + (index==this.itemIndex ? 'ok':'no')
             ctx.fillText(str, x + 10, y + h  / 2)
         },
         eventListener(evt){
-            /* 点击事件 */
-            //                console.log(evt); return true;
-            //                evt = evt.changedTouches[0]; //touchend
-            //                evt = evt.touches[0];   //touchstart
-            const mousePos = MyCanvas.getMousePos(this.canvas, evt, 1)
+            let _evt
 
-
+            if(evt.type=='touchstart'){
+                _evt = evt.touches[0];   //touchstart
+            }else if (evt.type == 'touchend'){
+                _evt = evt.changedTouches[0]; //touchend
+            }else if (evt.type == 'click') {
+                _evt = evt
+            }               
+            
+            const mousePos = MyCanvas.getMousePos(this.canvas, _evt, 1)
             //console.log("鼠标指针坐标：" + mousePos.x + "," + mousePos.y);
 
             function getListItemIndex(pos, listCount){
-
-                let index = -1
-
+                let index = 0
                 let x1 = RLCParam.listX
-
                 let x2 = RLCParam.listItemW + RLCParam.listX
-
-
                 if(pos.x >= x1 && pos.x <= x2){
-                    //                        console.log(pos.x + ':' + x1 + '~' + x2)
-
-                    let y1 = RLCParam.listY
-                    let y2 = y1 + RLCParam.listItemH
+                    let y1, y2
 
                     for(let i = 1 ; i <= listCount ; i++ ){
-                        //                            console.log(pos.y + ':' + y1 + '~' + y2)
-
+                        y1 = RLCParam.listY + parseInt(RLCParam.listItemH + RLCParam.listItemPad) * ( i - 1)
+                        y2 = y1 + RLCParam.listItemH    
                         if( pos.y >= y1 && pos.y <= y2){
                             index = i
                             break
                         }
-
-                        y1 = y1 + RLCParam.listItemH + RLCParam.listItemPad
-
-                        y2 = y1 + RLCParam.listItemH
-
                     }
                 }
-
                 return index
             }
 
-            let itemIndex = getListItemIndex(mousePos,this.count)
+            const itemIndex = getListItemIndex(mousePos,this.count)
 
-            if(itemIndex > 0 && itemIndex <= this.count) {
-                this.$store.dispatch('myRoom/Enter',itemIndex).then(()=>{
-                    return this.$store.dispatch('myRoom/GetInfo',{force:true})
-                })
+            this.drawTest(this.ctx, mousePos, itemIndex)
+
+            if(evt.type == 'touchstart'){
+                this.itemIndex = itemIndex
+
+                this.drawItem(this.ctx, this.itemIndex, true)
+            }else if(evt.type == 'touchend'){
+                this.drawItem(this.ctx, this.itemIndex, false)
+
+                if(itemIndex > 0 && itemIndex == this.itemIndex && itemIndex <= this.count) {
+                    this.enter(itemIndex)
+                }else{
+                    this.itemIndex = 0
+                }
             }
-            //                this.drawTest(this.ctx, mousePos, itemIndex)
+        },
+        async enter(index){
+            await this.$store.dispatch('myRoom/Enter',index)
+            await this.$store.dispatch('myRoom/GetInfo',{force:true})
         }
     },
     destroyed(){
