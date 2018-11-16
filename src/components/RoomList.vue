334450f20ee18deb6964db7130558e772007ce13
doDomoGu <kaka_71936410@qq.com>
2018-11-16 14:25:48 +0800

    房间列表点击渐变效果  暂未完成

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -80,31 +80,90 @@ export default {
             }
         },
         //绘制单个
-        drawItem(ctx, index, actived){
+        drawItem(ctx, index, actived, transition = false){
             let w = RLCParam.listItemW
             let h = RLCParam.listItemH
             let pad = RLCParam.listItemPad
             let x = RLCParam.listX
             let y = RLCParam.listY + parseInt(index - 1) * parseInt(h + pad)
+
             let txt = index + '. ' + this.list[index - 1].title
             
             let bgcolor, txtcolor
-            if(actived){
-                bgcolor = '#333333'
-                txtcolor = '#fefefe'
+
+            const colorTxt = 35   //起始颜色值
+            const len = 10      //层次长度
+            const step = 22     //步长
+            const colorBg = colorTxt + len * step  //结束颜色值
+
+            const cTxt = colorTxt.toString(16), cBg = colorBg.toString(16)
+            
+            if(transition) { //渐隐渐显过渡效果
+                /* const t = async()=>{
+                    await setTimeout(()=>{
+                        ctx.fillStyle = bgcolor
+                        ctx.fillRect(x, y, w, h)
+                        
+                        ctx.font = MyCanvas.px2Rem(24) + 'px Microsoft JhengHei'
+                        ctx.fillStyle = txtcolor
+                        ctx.textAlign = 'left'
+                        ctx.textBaseline = 'middle'
+                        ctx.fillText(txt, x + 10, y + h  / 2)
+                    },200)
+                }
+
+
+                const s = async ()=>{
+                    let c2Bg,c2Txt
+
+                    for(let i = 0; i < len; i++){
+                        console.log(i)
+                        if(actived) {
+                            c2Bg = cTxt - i * step
+                            c2Txt = cBg + i * step
+                        }else{
+                            c2Bg = cBg + i * step
+                            c2Txt = cTxt - i * step
+                        }
+
+                        bgcolor = '#'+c2Bg+c2Bg+c2Bg
+                        txtcolor = '#'+c2Txt+c2Txt+c2Txt
+
+                        
+                        await t()
+                    }
+                }
+
+                s() */
+
+                
+                
+
             }else{
-                bgcolor = '#fefefe'
-                txtcolor  = '#4b4b4b' 
+
+                const activedBgcolor    = '#'+cBg+cBg+cBg,
+                    activedTxtcolor     = '#'+cTxt+cTxt+cTxt,
+                    unactivedBgcolor    = activedBgcolor,
+                    unactivedTxtcolor   = activedTxtcolor
+
+                if(actived){
+                    bgcolor = activedBgcolor
+                    txtcolor = activedTxtcolor
+                }else{
+                    bgcolor = unactivedBgcolor
+                    txtcolor  = unactivedTxtcolor
+                }
+                ctx.fillStyle = bgcolor
+                ctx.fillRect(x, y, w, h)
+                
+                ctx.font = MyCanvas.px2Rem(24) + 'px Microsoft JhengHei'
+                ctx.fillStyle = txtcolor
+                ctx.textAlign = 'left'
+                ctx.textBaseline = 'middle'
+                ctx.fillText(txt, x + 10, y + h  / 2)
             }
 
-            ctx.fillStyle = bgcolor
-            ctx.fillRect(x, y, w, h)
             
-            ctx.font = MyCanvas.px2Rem(24) + 'px Microsoft JhengHei'
-            ctx.fillStyle = txtcolor
-            ctx.textAlign = 'left'
-            ctx.textBaseline = 'middle'
-            ctx.fillText(txt, x + 10, y + h  / 2)
         },
         //test 绘制点击信息
         drawTest(ctx, pos, index){
